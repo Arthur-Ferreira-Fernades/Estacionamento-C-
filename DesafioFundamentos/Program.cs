cf17a6244c2134c882b57ab1eab5e89140ce81ad
Arthur Ferreira <arthurfernandesferreira@hotmail.com>
2023-10-17 12:09:48 -0300

    Alterações realizadas

--- DesafioFundamentos/Program.cs
+++ DesafioFundamentos/Program.cs
@@ -23,14 +23,15 @@ bool exibirMenu = true;
 while (exibirMenu)
 {
     Console.Clear();
+    
     Console.WriteLine("Digite a sua opção:");
     Console.WriteLine("1 - Cadastrar veículo");
     Console.WriteLine("2 - Remover veículo");
     Console.WriteLine("3 - Listar veículos");
     Console.WriteLine("4 - Encerrar");
 
-    switch (Console.ReadLine())
-    {
+    switch (Console.ReadLine()) {
+
         case "1":
             es.AdicionarVeiculo();
             break;
@@ -50,6 +51,7 @@ while (exibirMenu)
         default:
             Console.WriteLine("Opção inválida");
             break;
+
     }
 
     Console.WriteLine("Pressione uma tecla para continuar");
